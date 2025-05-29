461cd695167516e769ccb18bfe929bd6adfb2595
Breno Mingrone <breno.maragno@outlook.com>
2025-05-29 14:22:02 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+  //if(html.classList.contains('light')) {
+    //html.classList.remove('light')
+  //}
+  //else {
+    //html.classList.add('light')
+  //}
+
+     //se tiver light mode:
+     //if(html.classList.contains('light')) {
+      //img.setAttribute('src', './assets/avatar-light.png')}
+
+      //se não tiver live mode: 
+      // else{
+      //img.setAttribute('src', './assets/avatar,png')}
+
+      
+}
